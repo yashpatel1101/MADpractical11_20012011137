5f2eb167df51a91aeae3a861d104d884d66e14e3
YASH <yashpatelys1101@gmail.com>
2022-11-18 12:05:59 +0530

    Practical 11

---
+++ app/src/main/java/com/example/madpractical11_20012011137/NotesData.kt
@@ -0,0 +1,24 @@
+package com.example.madpractical11_20012011137
+
+class NotesData {
+    companion object{
+        const val TABLE_NAME = "notes"
+
+        const val COLUMN_ID = "id"
+        const val COLUMN_NOTE_TITLE = "note_title"
+        const val COLUMN_NOTE_SUB_TITLE = "note_sub_title"
+        const val COLUMN_NOTE_DESCRIPTION = "note_description"
+        const val COLUMN_NOTE_SET_REMINDER = "note_set_reminder"
+        const val COLUMN_NOTE_REMINDER_TIME = "note_set_reminder_time"
+        const val COLUMN_TIMESTAMP = "note_modified_timestamp"
+
+        val CREATE_TABLE = ("CREATE TABLE "+ TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NOTE_TITLE + " TEXT,"
+                + COLUMN_NOTE_SUB_TITLE + " TEXT,"
+                + COLUMN_NOTE_DESCRIPTION + " TEXT,"
+                + COLUMN_NOTE_SET_REMINDER + " INTEGER,"
+                + COLUMN_NOTE_REMINDER_TIME + " INTEGER,"
+                + COLUMN_TIMESTAMP + " TEXT" + ")")
+    }
+}
